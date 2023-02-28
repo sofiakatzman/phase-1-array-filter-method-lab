db07987775a6e920f5a19b7509cdaf3ec90a104c
Sofia Katzman <katzmansof@gmail.com>
2023-02-28 01:02:21 -0500

    finished lab with completed functions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+// find drivers based on entire string - no case match
+
+function findMatching(drivers, stringComparison) {
+    return drivers.filter(function (driver) { return driver.toLowerCase()=== stringComparison.toLowerCase() })
+  }
+
+
+  // find drivers using a piece of a string - no case match
+  function fuzzyMatch(drivers, stringComparison) {
+    return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, stringComparison.length) === stringComparison.toLowerCase() })
+  }
+
+
+// looks inside array and finds match 
+  function matchName(drivers, stringComparison) {
+    return drivers.filter(function (driver) { return driver.name === stringComparison })
+  }
